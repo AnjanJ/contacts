f9c15207a1532fa17810fa8db270b25633dde8e6
Anjan Jagirdar <anjan.jagirdar@gmail.com>
2020-06-21 16:43:23 +0530

    fix typo in validation column names

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,4 @@
 class Contact < ApplicationRecord
-    validates_presence_of :first_name, :last_name, :email, :text
+    validates_presence_of :first_name, :last_name, :email, :message
     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
