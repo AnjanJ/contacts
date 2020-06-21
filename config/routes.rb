2ce7788c8e1e57191e9ca1a1bba4e4d8ff03abee
Anjan Jagirdar <anjan.jagirdar@gmail.com>
2020-06-21 12:54:55 +0530

    add route & catch all routes to contacts create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'contacts/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'contacts#create'
+  post 'contacts', to: 'contacts#create'
+
+  # this route should be at the bottom always
+  match "*path", to: "contacts#create", via: :all
 end
