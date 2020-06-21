8560a1a1688bb11f4b5923d58a7bcae8b745fb90
Anjan Jagirdar <anjan.jagirdar@gmail.com>
2020-06-21 22:26:08 +0530

    Add russian language supprt & scope routes by locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'contacts#new'
-  resources :contacts, only: [:new, :create]
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root to: 'contacts#new'
+    resources :contacts, only: [:new, :create]
 
-  # this route should be at the bottom always
-  match "*path", to: "contacts#new", via: :all
+    # this route should be at the bottom always
+    match "*path", to: "contacts#new", via: :all
+  end
 end
