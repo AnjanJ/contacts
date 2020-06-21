f7b51b22642845900e81f755fa5ca7c36d6de233
Anjan Jagirdar <anjan.jagirdar@gmail.com>
2020-06-21 16:45:17 +0530

    correct route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'contacts/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'contacts#create'
-  post 'contacts', to: 'contacts#create'
+  root to: 'contacts#new'
+  resources :contacts, only: [:new, :create]
 
   # this route should be at the bottom always
-  match "*path", to: "contacts#create", via: :all
+  match "*path", to: "contacts#new", via: :all
 end
