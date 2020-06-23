ef9b7bf83a1b1ab2a95c17b5b9420fbc56179217
Anjan Jagirdar <anjan.jagirdar@gmail.com>
2020-06-23 15:09:33 +0530

    refactor set locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,7 @@ class ApplicationController < ActionController::Base
     private
 
     def set_locale
-        I18n.locale = extract_locale || I18n.default_locale
-    end
-
-    def extract_locale
-        parsed_locale = params[:locale]
-        I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+        I18n.locale = I18n.available_locales.map(&:to_s).include?(params[:locale]) ? params[:locale] : I18n.default_locale
     end
 
     def default_url_options
