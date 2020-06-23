a752b8d8f596acb74dd86efb55cbe3583655c7d8
Anjan Jagirdar <anjan.jagirdar@gmail.com>
2020-06-23 22:36:00 +0530

    move notification email to callback

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,10 @@
 class Contact < ApplicationRecord
     validates_presence_of :first_name, :last_name, :message
     validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+    after_create_commit :notify_about_new_contact
+
+    def notify_about_new_contact
+        ContactMailer.with(contact: self).new_contact_email.deliver_later
+    end
 end
