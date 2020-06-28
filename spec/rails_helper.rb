7109effc5ca618e167c8cdacfa74c4f4bf3fb1eb
Anjan Jagirdar <anjan.jagirdar@gmail.com>
2020-06-28 19:22:19 +0530

    add coverage for rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -62,7 +66,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   # setting for rspec and shoulda matchers
-  Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+  Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
